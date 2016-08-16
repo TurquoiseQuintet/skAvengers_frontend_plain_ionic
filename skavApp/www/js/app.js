cb47b6ed12d692b0e805710b9c405eff3ea6afcb
Roy <rechase84@gmail.com>
2016-08-16 13:00:55 -0600

    struggling with serving states

--- skavApp/www/js/app.js
+++ skavApp/www/js/app.js
@@ -1,39 +1,41 @@
 'use strict';
 
 var app = angular.module('skavApp', ['ionic']);
-
-app.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
-
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
-  });
-
-app.config(function($stateProvider){
+app.config(function($stateProvider) {
   $stateProvider
-    .state('admin', {
-    url: '/admin',
-    templateUrl: 'templates/admin.html'
+  .state('index', {
+    url: '/',
+    templateUrl: '/templates/login.html'
   })
   .state('register', {
     url: '/register',
-    templateUrl: 'templates/register.html'
+    templateUrl: '/templates/register.html'
   })
-  .state('login', {
-    url: '/',
-    templateUrl: 'templates/login.html'
+  .state('admin',{
+    url: '/admin',
+    templateUrl: '/templates/admin.html'
   });
-
 });
-// Each state's controller can be found in controllers.js
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+    }
+  });
+});
+
+
+
+
 
 // $urlRouterProvider.otherwise('/');
