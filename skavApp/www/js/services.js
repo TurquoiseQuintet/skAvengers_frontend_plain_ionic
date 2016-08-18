6115638584dc8813ded484742e3f045ed4e109ed
Roy <rechase84@gmail.com>
2016-08-18 09:42:12 -0600

    controller had a typo

--- skavApp/www/js/services.js
+++ skavApp/www/js/services.js
@@ -1,13 +1,13 @@
 'use strict';
 // sign up service ---------------------------->
-app.service('SignUpService', ['$http', '$window', '$location', function($http, $window, $location){
+app.service('SignUpService', ['$http', '$window', '$state', function($http, $window, $state){
   var sv=this;
 sv.signup= function(username, password, email, avatar){
   $http.post('https://skavengers.herokuapp.com/register', {username:username, password:password, email:email, avatar:avatar})
   .then(function(response){
     console.log(response);
     //path to login or does signup log you in and path to user home?
-    $location.path('/user');
+    $state.go('user');
   })
   .catch(function(err){
     console.log(err);
